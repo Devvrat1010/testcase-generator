beb8ebec9af3727025a9c73f93c214dd93fdca53
Devvrat1010 <rathoddevvratsingh777@gmail.com>
2024-07-09 14:58:06 +0530

    first commit

---
+++ src/app/generateTestcases/page.tsx
@@ -0,0 +1,7 @@
+export default function GenerateTestCase(){
+    return (
+        <div className="h-screen w-screen bg-black text-white">
+            <input type="text" name="" id="" />
+        </div>
+    );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+
+
+
+export default function Home() {
+    return (
+        <main className="h-screen w-screen bg-black text-white">
+            
+        </main>
+
+    );
+}
