53cd3bc3bf75c18591440c87a390217893e914e4
Devvrat1010 <rathoddevvratsingh777@gmail.com>
2024-12-27 19:05:14 +0530

    test

--- backend/src/main/java/testcasegenerator/TestcaseApplication.java
+++ backend/src/main/java/testcasegenerator/TestcaseApplication.java
@@ -1,8 +1,12 @@
 package testcasegenerator;
-
+// package com.koyeb.examplespringboot;
+ 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
 
 @SpringBootApplication
 public class TestcaseApplication {
